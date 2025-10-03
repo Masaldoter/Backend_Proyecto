2c67ba4aad042def5a7bac494a0e3cd0d2c01c0b
Masaldoter <aldoyagu@gmail.com>
2025-10-02 21:46:28 -0600

    V1.2

--- Portafolio_Backend/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
+++ Portafolio_Backend/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7244c34ef303b561627770bac198fc4a3425ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ac67df0ad8609d836482ccc0ddb21d14fa9b36")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
